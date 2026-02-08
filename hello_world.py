4b25296afdc2c88f0481825dc54e80eea45e0e92
Sameer Shalayel <105084368+samshalayel@users.noreply.github.com>
2026-02-08 15:59:25 +0200

    Add hello world Python script for Gaza project

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Hello World Python Script
+A simple Python program to print "Hello World"
+Created for Gaza project
+"""
+
+def main():
+    """Main function to print Hello World"""
+    print("Hello World!")
+    print("مرحباً بالعالم!")  # Arabic greeting
+    print("Welcome to Gaza Project")
+
+if __name__ == "__main__":
+    main()
